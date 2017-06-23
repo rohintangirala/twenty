abf3ff62f6e608ff5c7ad14337cbac5fa47ebd08
Rohin Tangirala <rohinkt@gmail.com>
2017-06-22 20:45:38 -0700

    Initial Commit

---
+++ Twenty-Desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twenty-Desktop
+//
+//  Created by Rohin Tangirala on 6/22/17.
+//  Copyright © 2017 Rohin Tangirala. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Twenty-Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twenty-Desktop
+//
+//  Created by Rohin Tangirala on 6/22/17.
+//  Copyright © 2017 Rohin Tangirala. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
