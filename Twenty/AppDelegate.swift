ec7e6bac9f01d4dddc647d35646828fcbf7121d7
Rohin Tangirala <rohinkt@gmail.com>
2019-07-21 17:09:12 -0700

    format

--- Twenty/AppDelegate.swift
+++ Twenty/AppDelegate.swift
@@ -10,21 +10,20 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-      // Insert code here to initialize your application
-      NSUserNotificationCenter.default.delegate = self
-  }
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        NSUserNotificationCenter.default.delegate = self
+    }
 
-  func applicationWillTerminate(_ aNotification: Notification) {
-      // Insert code here to tear down your application
-  }
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
 
-  func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
-    return true
-  }
+    func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
+        return true
+    }
 
-  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
-
--- Twenty/BarChartViewController.swift
+++ Twenty/BarChartViewController.swift
@@ -10,68 +10,67 @@ import Cocoa
 import Charts
 
 class BarChartViewController: NSViewController {
-  let defaults: UserDefaults = UserDefaults.standard
-  var days: [String] = []
+    let defaults: UserDefaults = UserDefaults.standard
+    var days: [String] = []
 
-  @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var barChartView: BarChartView!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    // Do view setup here.
-    self.view.wantsLayer = true
-  }
-
-  override func viewDidAppear() {
-    defaults.synchronize()
-
-    // Get data from UserDefaults
-    if let _ : AnyObject = (defaults.object(forKey: "usageTimes") as AnyObject??)! {
-      let mins = defaults.object(forKey: "usageTimes") as AnyObject?? as! [Double]
-
-      if let _ : AnyObject = (defaults.object(forKey: "usageDates") as AnyObject??)! {
-        let dates = defaults.object(forKey: "usageDates") as AnyObject?? as! [String]
-        setChart(dataPoints: dates, values: mins)
-      }
-    }
-  }
-
-  // Create bar graph and initialize with values
-  func setChart(dataPoints: [String], values: [Double]) {
-    barChartView.clearValues()
-    var dataEntries: [BarChartDataEntry] = Array()
-    var counter = 0.0
-
-    for i in 0..<dataPoints.count {
-      counter += 1.0
-      let dataEntry = BarChartDataEntry(x: counter, y: values[i])
-      dataEntries.append(dataEntry)
+        // Do view setup here.
+        self.view.wantsLayer = true
     }
 
-    let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Usage in minutes of each working period")
-    chartDataSet.setColors(
-      NSUIColor(calibratedRed: 0.6, green: 0, blue: 0.69, alpha: 1.0),
-      NSUIColor(calibratedRed: 1.0, green: 0, blue: 0.38, alpha: 1.0),
-      NSUIColor(calibratedRed: 0.22, green: 0, blue: 0.98, alpha: 1.0)
-    )
+    override func viewDidAppear() {
+        defaults.synchronize()
 
-    let chartData = BarChartData()
-    chartData.addDataSet(chartDataSet)
-    chartDataSet.drawValuesEnabled = false
-    self.barChartView.data = chartData
-    self.barChartView.leftAxis.labelTextColor = NSUIColor.white
-    self.barChartView.rightAxis.drawLabelsEnabled = false
-    self.barChartView.xAxis.drawGridLinesEnabled = false
-    self.barChartView.leftAxis.drawGridLinesEnabled = false
-    self.barChartView.rightAxis.drawGridLinesEnabled = false
-    self.barChartView.leftAxis.axisMinimum = 0.0
-    self.barChartView.xAxis.drawAxisLineEnabled = false
-    self.barChartView.leftAxis.drawAxisLineEnabled = false
-    self.barChartView.rightAxis.drawAxisLineEnabled = false
-    self.barChartView.legend.enabled = false
-    self.barChartView.xAxis.drawLabelsEnabled = false
-    self.barChartView.chartDescription?.text = "";
-    self.barChartView.noDataTextColor = NSUIColor.white
-  }
+        // Get data from UserDefaults
+        if let _ : AnyObject = (defaults.object(forKey: "usageTimes") as AnyObject??)! {
+            let mins = defaults.object(forKey: "usageTimes") as AnyObject?? as! [Double]
+
+            if let _ : AnyObject = (defaults.object(forKey: "usageDates") as AnyObject??)! {
+                let dates = defaults.object(forKey: "usageDates") as AnyObject?? as! [String]
+                setChart(dataPoints: dates, values: mins)
+            }
+        }
+    }
+
+    // Create bar graph and initialize with values
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.clearValues()
+        var dataEntries: [BarChartDataEntry] = Array()
+        var counter = 0.0
+
+        for i in 0..<dataPoints.count {
+            counter += 1.0
+            let dataEntry = BarChartDataEntry(x: counter, y: values[i])
+            dataEntries.append(dataEntry)
+        }
+
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Usage in minutes of each working period")
+        chartDataSet.setColors(
+            NSUIColor(calibratedRed: 0.6, green: 0, blue: 0.69, alpha: 1.0),
+            NSUIColor(calibratedRed: 1.0, green: 0, blue: 0.38, alpha: 1.0),
+            NSUIColor(calibratedRed: 0.22, green: 0, blue: 0.98, alpha: 1.0)
+        )
+
+        let chartData = BarChartData()
+        chartData.addDataSet(chartDataSet)
+        chartDataSet.drawValuesEnabled = false
+        self.barChartView.data = chartData
+        self.barChartView.leftAxis.labelTextColor = NSUIColor.white
+        self.barChartView.rightAxis.drawLabelsEnabled = false
+        self.barChartView.xAxis.drawGridLinesEnabled = false
+        self.barChartView.leftAxis.drawGridLinesEnabled = false
+        self.barChartView.rightAxis.drawGridLinesEnabled = false
+        self.barChartView.leftAxis.axisMinimum = 0.0
+        self.barChartView.xAxis.drawAxisLineEnabled = false
+        self.barChartView.leftAxis.drawAxisLineEnabled = false
+        self.barChartView.rightAxis.drawAxisLineEnabled = false
+        self.barChartView.legend.enabled = false
+        self.barChartView.xAxis.drawLabelsEnabled = false
+        self.barChartView.chartDescription?.text = "";
+        self.barChartView.noDataTextColor = NSUIColor.white
+    }
 }
-
